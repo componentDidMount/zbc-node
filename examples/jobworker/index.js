3b342a3199c6c4ec4e220ffc96b195bcf9ee1010
sam <psam.dev@gmail.com>
2018-05-24 22:22:31 +0200

    chore(review) Fixed tiny stuff

--- examples/jobworker/index.js
+++ examples/jobworker/index.js
@@ -1,48 +1,47 @@
-const { Client } = require("zbc-nodejs")
+const { Client } = require('zbc-nodejs')
 
-var myClient = new Client({bootstrapAddr: "0.0.0.0:51015"})
-console.log("starting client")
+var myClient = new Client({bootstrapAddr: '0.0.0.0:51015'})
+console.log('starting client')
 
-myClient.createTopic("hello-world")
-myClient.createWorkflow("hello-world", "../demoProcess.bpmn")
+myClient.createTopic('hello-world')
+myClient.createWorkflow('hello-world', '../demoProcess.bpmn')
 
-console.log("creating workflow instances")
+console.log('creating workflow instances')
 for (let i = 0; i < 100; i++) {
-  myClient.createWorkflowInstance("hello-world", "demoProcess", myClient.LatestVersion, {"a": "b"})
+  myClient.createWorkflowInstance('hello-world', 'demoProcess', myClient.LatestVersion, {'a': 'b'})
 }
 
 let counter = 0
-console.log("starting job worker")
-myClient.jobWorker("default-topic", "foo", {}, function(jobKey, payload) {
-    counter++
+console.log('starting job worker')
+myClient.jobWorker('default-topic', 'foo', {}, function (jobKey, payload) {
+  counter++
 
-    console.log()
-    console.log(counter, jobKey, payload)
-    if (counter % 2 == 0) {
-      console.log("Completing job!")
-      myClient.completeJob(jobKey, payload)
-      return
-    }
-    
-    console.log("Failing job!")
-    myClient.failJob(jobKey)
-  
+  console.log()
+  console.log(counter, jobKey, payload)
+  if (counter % 2 == 0) {
+    console.log('Completing job!')
+    myClient.completeJob(jobKey, payload)
+    return
+  }
+
+  console.log('Failing job!')
+  myClient.failJob(jobKey)
 })
 
-if (process.platform === "win32") {
-  var rl = require("readline").createInterface({
+if (process.platform === 'win32') {
+  var rl = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
-  });
+  })
 
-  rl.on("SIGINT", function () {
-    process.emit("SIGINT");
-  });
+  rl.on('SIGINT', function () {
+    process.emit('SIGINT')
+  })
 }
 
-process.on("SIGINT", function () {
-  //graceful shutdown
-  process.exit();
-});
+process.on('SIGINT', function () {
+  // graceful shutdown
+  process.exit()
+})
 
 console.log("I'm done!")
