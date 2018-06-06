5e3efd2b7e24de784d00ada0c541749a479de7d4
Sebastian Menski <sebastian.menski@camunda.com>
2018-06-06 11:30:27 +0200

    fix(get-started): fix process id and payload name

--- examples/zeebe-js-getstarted/receiveOrders.js
+++ examples/zeebe-js-getstarted/receiveOrders.js
@@ -2,6 +2,6 @@ const { Client } = require('zbc-node')
 
 const zbClient = new Client({bootstrapAddr: '0.0.0.0:51015'})
 
-const payload = {'orderNumber': '12345'}
-const createInstanceResult = zbClient.createWorkflowInstance('get-started', 'orderProcess', zbClient.LatestVersion, payload)
+const payload = {'orderId': '12345'}
+const createInstanceResult = zbClient.createWorkflowInstance('get-started', 'order-process', zbClient.LatestVersion, payload)
 console.log(createInstanceResult)
